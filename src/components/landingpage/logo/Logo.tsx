5674f593ee1cc01ce7b950b2320de4bd38d7428f
Andrea Fan <andrea.fan@ringus-solution.com>
2023-02-09 18:01:22 +0800

    Update: Setting up routes and constants

--- src/components/landingpage/logo/Logo.tsx
+++ src/components/landingpage/logo/Logo.tsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const Logo = (): JSX.Element => {
   return (
     <div>
       <Link to="/">Glance</Link>
+      <span></span>
     </div>
   );
 };
--- src/style/styles.ts
+++ src/style/styles.ts
@@ -17,18 +17,33 @@ const DARK_MODE_COLORS = {
 
 const MARGIN = {
   marginXS: 6,
-  marginS: 10,
-  marginM: 16,
-  marginL: 20,
-  marginXL: 26,
+  marginS: 16,
+  marginM: 20,
+  marginL: 26,
+  marginXL: 30,
 };
 
 const PADDING = {
   paddingXS: 6,
-  paddingS: 10,
-  paddingM: 16,
-  paddingL: 20,
-  paddingXL: 26,
+  paddingS: 16,
+  paddingM: 26,
+  paddingL: 36,
+  paddingXL: 46,
 };
 
-export { COLORS, LIGHT_MODE_COLORS, DARK_MODE_COLORS, MARGIN, PADDING };
+const FONTSIZE = {
+  fontXS: 6,
+  fontS: 16,
+  fontM: 26,
+  fontL: 36,
+  fontXL: 46,
+};
+
+export {
+  COLORS,
+  LIGHT_MODE_COLORS,
+  DARK_MODE_COLORS,
+  MARGIN,
+  PADDING,
+  FONTSIZE,
+};
